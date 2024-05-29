cc016b60705202eac873e2bf0c083bbe071d2a46
Andrew <asoborev@mail.ru>
2024-05-29 21:05:05 +0300

    навигация по клику с главной если не зарегистрирован

--- src/components/Warning/Warning.tsx
+++ src/components/Warning/Warning.tsx
@@ -1,13 +1,19 @@
+import { useNavigate } from 'react-router';
 import Button from '../Button/Button';
 import styles from './warning.module.css';
 
 const Warning = () => {
+    const navigate = useNavigate();
+    const handleNavigate = () => {
+        navigate('/greeting');
+    };
+
     return (
         <div className={styles.warning}>
             <h2 className={styles.warningHeader}>
                 to create transactions, you need to register
             </h2>
-            <Button>Registration</Button>
+            <Button callback={handleNavigate}>Registration</Button>
         </div>
     );
 };
