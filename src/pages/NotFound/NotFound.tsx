ce9c03457cd387d917e9a680fb41b792d9d87c7a
Andrew <asoborev@mail.ru>
2024-05-29 21:22:33 +0300

    реализована страница notFound

--- src/pages/NotFound/NotFound.tsx
+++ src/pages/NotFound/NotFound.tsx
@@ -1,5 +1,23 @@
+import { useNavigate } from 'react-router';
+import Button from '../../components/Button/Button';
+import styles from './notFound.module.css';
+
 const NotFound = () => {
-    return <h2>not found</h2>;
+    const navigate = useNavigate();
+
+    const handleNavigate = () => {
+        navigate('/');
+    };
+
+    return (
+        <div className={styles.notFound}>
+            <div>
+                <h1>404</h1>
+                <h2>Page not found</h2>
+            </div>
+            <Button callback={handleNavigate}>go to main</Button>
+        </div>
+    );
 };
 
 export default NotFound;
